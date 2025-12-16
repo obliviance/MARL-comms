4076dab30982b50a684d388239e58dec934e8106
obliviance <awwab.mahdi@gmail.com>
2025-12-16 13:25:40 -0500

    Working on SARSA

--- mpe_policies.py
+++ mpe_policies.py
@@ -1,9 +1,18 @@
 import random
 import numpy as np
-from pettingzoo.mpe import simple_speaker_listener_v4
 
 def random_policy(observation, action_space, reward):
     return random.choice(range(action_space.n))
+
+def sarsa(observation, action_space, reward):
+    # Simple SARSA policy
+    q_table = np.zeros((observation.shape[0], observation.shape[1], action_space.n))
+    state = tuple(observation)
+    if random.random() < 0.1:  # exploration
+        action = random.choice(range(action_space.n))
+    else:  # exploitation
+        action = np.argmax(np.random.rand(action_space.n))  # placeholder for Q-values
+    return action
 
 def q_learning(observation, action_space, reward):
     # Simple Q-learning policy
@@ -16,10 +25,5 @@ def q_learning(observation, action_space, reward):
     else:  # exploitation
         action = np.argmax(q_table[state])
     return action
-
-
-def dqn_policy(observation, action_space, reward):
-    # Placeholder for DQN policy implementation
-    pass
 
 # Other policy implementations ...
